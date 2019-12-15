dbaf974b2b6c31d8d33e73d94e1398f11cc7d440
Jmo335 <54688404+Jmo335@users.noreply.github.com>
2019-12-14 19:09:19 -0500

    data

---
+++ Slones/potree/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 529628,
+    "boundingBox": {
+        "lx": 5712895.88,
+        "ly": 3792906.85,
+        "lz": 882.97,
+        "ux": 5714895.87,
+        "uy": 3794906.8400000005,
+        "uz": 2882.9600000002238
+    },
+    "tightBoundingBox": {
+        "lx": 5712895.88,
+        "ly": 3792906.85,
+        "lz": 882.97,
+        "ux": 5714895.87,
+        "uy": 3794906.84,
+        "uz": 1270.8
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 17.32042121887207,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
